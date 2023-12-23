cbffb8b4a8cf4085721df89eba043a8f090bd876
feichi <2112510964@qq.com>
2023-12-24 00:04:57 +0800

    15. vue3+skuComponent packaging ,im component communication

--- vue/code/vue2_vue3_basics/day13/sku_detail/.eslintrc.cjs
+++ vue/code/vue2_vue3_basics/day13/sku_detail/.eslintrc.cjs
@@ -3,7 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
@@ -11,24 +11,24 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   },
-  rules:{
+  rules: {
     'prettier/prettier': [
-        'warn',
-        {
-          singleQuote: true, // 单引号
-          semi: false, // 无分号
-          printWidth: 80, // 每行宽度至多80字符
-          trailingComma: 'none', // 不加对象|数组最后逗号
-          endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
-        }
-      ],
-      'vue/multi-word-component-names': [
-        'warn',
-        {
-          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
-        }
-      ],
-      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
-      // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭
+      'warn',
+      {
+        singleQuote: true, // 单引号
+        semi: false, // 无分号
+        printWidth: 80, // 每行宽度至多80字符
+        trailingComma: 'none', // 不加对象|数组最后逗号
+        endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
+      }
+    ],
+    'vue/multi-word-component-names': [
+      'warn',
+      {
+        ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+      }
+    ],
+    'vue/no-setup-props-destructure': ['off'] // 关闭 props 解构的校验
+    // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭
   }
 }
